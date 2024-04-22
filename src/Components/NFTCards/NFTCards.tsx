6f8d5e2a8a94cc9dc8a4d7ad7b58853086a7151c
mukhtar <mukhtarkabirmuhammad32@gmail.com>
2024-04-23 00:30:46 +0100

    update

--- src/Components/NFTCards/NFTCards.tsx
+++ src/Components/NFTCards/NFTCards.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import "./NFTCards.css";
 
+// Define the props interface for NFTCard component
 interface NFTCardProps {
-  nft: any;
+  nft: any; // NFT object
 }
 
+// NFTCard functional component
 const NFTCard: React.FC<NFTCardProps> = ({ nft }) => {
-    console.log("NFT Object:", nft);
+  // Log the NFT object for debugging purposes
+  console.log("NFT Object:", nft);
 
+  const imageCID = nft?.template?.immutable_data?.img;
+
+  // Render the NFTCard component
   return (
     <div className="nft-card">
+      {/* Render the name of the NFT */}
       <h3>{nft.name}</h3>
-      <img src={`https://ipfs.io/ipfs/${nft.image}`} alt={nft.name} />
+      {/* Render the image of the NFT */}
+      <img src={`https://ipfs.io/ipfs/${nft.img}`} alt={nft.data.name} />
+      {/* Render the mint number of the NFT if available */}
       {nft.template_mint && <p>Mint Number: {nft.template_mint}</p>}
     </div>
   );
 };
 
-export default NFTCard;
+export default NFTCard; // Export the NFTCard component
 
 
--- src/Components/NFTCollections/NFTCollections.tsx
+++ src/Components/NFTCollections/NFTCollections.tsx
@@ -2,22 +2,33 @@ import React from 'react';
 import NFTCard from '../NFTCards/NFTCards';
 import "./NFTCollections.css"
 
+// Define the props interface for NFTCollections component
 interface NFTCollectionsProps {
-  nfts: any[];
+  nfts: any[]; // Array of NFTs
 }
 
+// NFTCollections functional component
 const NFTCollections: React.FC<NFTCollectionsProps> = ({ nfts }) => {
+  // If there are no NFTs, render a message indicating so
   if (!nfts) {
     return <div>No NFTs found.</div>;
   }
 
+  // Render the NFT collections
   return (
     <div className="nft-collections">
+      {/* Map through each NFT collection */}
       {nfts.map((nft, index) => (
         <div key={index}>
+          {/* Render the name of the NFT collection */}
           <h2>{nft.collection.name}</h2>
+          <img src={`https://ipfs.io/ipfs/${nft.collection.img}`} alt="" />
+          <p>{nft.template_mint}</p>
+          {/* Render the NFT cards for the current collection */}
           <div className="nft-cards">
+            {/* Map through each asset in the collection */}
             {nft.assets && nft.assets.map((asset: any, idx: number) => (
+              // Render the NFTCard component for each asset
               <NFTCard key={idx} nft={asset} />
             ))}
           </div>
@@ -27,6 +38,4 @@ const NFTCollections: React.FC<NFTCollectionsProps> = ({ nfts }) => {
   );
 };
 
-export default NFTCollections;
-
- 
+export default NFTCollections; // Export the NFTCollections component
